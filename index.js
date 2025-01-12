083b5a1b0ee8165bf9a26afae570109ad7e3dc21
deependra bishwakarma <--replace-all>
2025-01-12 22:02:45 +0545

    changes port

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-const PORT = 3000 || 3001;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
