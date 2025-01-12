d6a06eb5ba56508be886daa38fc17068d3849dde
deependra bishwakarma <--replace-all>
2025-01-12 22:07:23 +0545

    added live url to client

--- index.js
+++ index.js
@@ -54,5 +54,5 @@ io.on('connection', (socket) => {
 // Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on https://chatserver-zxbu.onrender.com`);
 });
